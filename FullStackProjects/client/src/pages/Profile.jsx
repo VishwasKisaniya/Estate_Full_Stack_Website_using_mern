5784fbdcdc189218222a563c9caf9386db812fe4
vishwaskisaniya <kisaniyavishwas@gmail.com>
2024-06-10 03:24:10 +0530

    added createListing UI page complete

--- FullStackProjects/client/src/pages/Profile.jsx
+++ FullStackProjects/client/src/pages/Profile.jsx
@@ -4,6 +4,7 @@ import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/st
 import { app } from "../firebase";
 import { updateUserFailure,updateUserSuccess, updateUserStart, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart, signOutUserFailure, signOutUserSuccess } from "../redux/user/userSlice.js";
 import { useDispatch } from "react-redux";
+import {Link} from 'react-router-dom';
 
 export default function Profile() {
   const fileRef = useRef(null);
@@ -150,6 +151,9 @@ const handleSignOut= async () =>{
         <button disabled ={loading} className=' bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80' onChange={handleChange}>
         {loading ? 'Loading...' : 'Update'}
         </button>
+        <Link className=' bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95' to ={"/create-listing"}>
+        Create Listings
+        </Link>
 
         <div className=' flex justify-between mt-5'>
           <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete Account</span>
