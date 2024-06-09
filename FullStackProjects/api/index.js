da4e8d7a4b011ef5d9f9545f1de6cc1d6c0d1fc1
vishwaskisaniya <kisaniyavishwas@gmail.com>
2024-06-10 02:28:37 +0530

    adde listing created api route

--- FullStackProjects/api/index.js
+++ FullStackProjects/api/index.js
@@ -3,7 +3,9 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser';
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -24,8 +26,8 @@ app.listen(3001, () => {
 //api routes
 
 app.use("/api/user", userRouter);
-
 app.use("/api/auth",authRouter);
+app.use("/api/listing",listingRouter);
 
 // middleware
 
